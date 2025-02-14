4cbd65c27814875d1f6a3a132d41e50f943b4787
Ian Chang <ywc261@nyu.edu>
2025-02-14 13:14:50 +0900

    ngrok implementation

--- src/llm.py
+++ src/llm.py
@@ -1,6 +1,11 @@
+import requests
 import ollama
 
+# llama3 only supports local usage. We'll need to think about using a free model on huggingface that can support cloud deployment
+# first, let's try using ngrok to use ollama and keep it running on my mac mini
+
 MODEL = "llama3"
+OLLAMA_SERVER_URL = "https://de99-112-220-73-130.ngrok-free.app"
 
 def generate_answer(context, question, chat_history):
     """
@@ -18,5 +23,11 @@ def generate_answer(context, question, chat_history):
     User: {question}
     Assistant:
     """
-    response = ollama.chat(model=MODEL, messages=[{"role": "system", "content": prompt}])
-    return response["message"]["content"]
+    response = requests.post(
+        OLLAMA_SERVER_URL,
+        json={
+            "model": MODEL,
+            "messages": [{"role": "system", "content": prompt}]
+        }
+    )
+    return response.json()["message"]["content"]
